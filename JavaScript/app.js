6db090e9d5fa8511ea89fdf0f3d114b00f9c6cac
Tyler Corbett <thetylercorbett@gmail.com>
2018-10-29 15:35:31 -0700

    converted app.js to work with todo variables

---
+++ JavaScript/app.js
@@ -0,0 +1,14 @@
+import todoApi from './todo-api.js';
+import todoList from './todo-list.js';
+import addTodo from './add-todo';
+
+const todos = todoApi.getAll();
+
+todoList.init(todos, function(todo) {
+    todoApi.remove(todo);
+});
+
+addTodo.init(function(todo) {
+    todoApi.add(todo);
+    todoList.add(todo);
+});
